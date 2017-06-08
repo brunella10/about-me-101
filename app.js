82d7ddc0111104567a410dfac257c32121fe3220
Gabriela Diez <gaby1010@gmail.com>
2017-06-07 21:18:12 -0700

    add loops and guess number game

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+
+var answerLoops = 0;
+var rightAnswer = 0;
+
 var userName = 'userName';
 
 var userName = prompt('What is your name?');
 alert('Very nice to meet you ' + userName );
 console.log('when asked user name, always respond')
 
+//question1
+
 var university = prompt('What university did you attend?');
 alert('Oh, amazing!');
 console.log('when user responds about a place, respond with amazing');
+
+//question2
 
 var degree = prompt('Did I finish College?'); // add yes or not question//
 console.log('if user did not answer, ask about something else');
@@ -19,7 +27,10 @@ if (degree === 'no') {
 } else {
   YesIdid = 'I graduate in 2007';
   alert(YesIdid);
+  rightAnswer++;
 }
+
+//question3
 
 var about = prompt('Did I like the university?');
 console.log('if the user said no, change the questions about the university to hobbies or plans');
@@ -29,12 +40,16 @@ if (about === "no") {
 } else {
   DisLike = 'Not always! I made some good friends there.';
   alert(DisLike);
+  rightAnswer++;
 }
 
-var study = prompt('Do I like you career?');
+//question4
+
+var study = prompt('Do I like my career?');
 console.log ('if the user answers no, ask why?');
 //add if else //
 if (study === "yes" || study === 'y') {
+  rightAnswer++;
   answer = 'I am lawyer, what can I say!? ';
   alert(answer);
 } else {
@@ -42,11 +57,38 @@ if (study === "yes" || study === 'y') {
   alert(answerNegative);
 }
 
-var vacation =prompt('Do I have plans to take a vacation after CodeFellows?');
+//question5
+
+var vacation = prompt('Do I have plans to take a vacation after CodeFellows?');
 if (vacation === "yes") {
   PlaceToGo = 'off course! we gonna need it!';
   alert(PlaceToGo);
 } else {
+  rightAnswer++;
   StayHome = 'I gonna spend the day sleeping! ZzZzZzZ ';
   alert(StayHome);
+}
+alert('You have'  + rightAnswer +  'rightAnswer' );
+//question6
+
+//booleans true or false questions
+
+var numberOfTries = 3;
+var moving = 6;
+
+while (numberOfTries > 0){
+  var guess = prompt('How many times did I move in the past 10 years?');
+
+  if (guess >  moving) {
+    alert('Too high');
+    console.log('if the guess is higher than moving variable let the user know the answer is to high.');
+  } else if (guess < moving) {
+    alert('Too low');
+    console.log('if the guess is lower than moving variable ler the user know the answer is too low');
+  } else {
+    alert('Yes! That is CORRECT!');
+    numberOfTries = -1;
+    console.log('If the guess is equal to moving variable the answer is correct');
+  }
+  numberOfTries = numberOfTries-1;
 }
